998ea244d8638ebe8655a20b92d6b39e5f4c5437
VictorAlencar00 <victordesousaa@gmail.com>
2024-06-09 15:15:27 -0300

    feat: search now works in lower case

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  RouterOutlet,
+  Router,
+  NavigationEnd,
+} from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
 import { HttpClientModule } from '@angular/common/http';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +18,27 @@ import { HttpClientModule } from '@angular/common/http';
 })
 export class AppComponent implements OnInit {
   title = 'gamestore';
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+  public showMenu: boolean = true;
   ngOnInit(): void {
-    this.route.url.subscribe((urlSegments) => {
-      const url = urlSegments.map((segment) => segment.path).join('/');
-      console.log(url);
-    });
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        let currentRoute = this.route;
+        while (currentRoute.firstChild) {
+          currentRoute.firstChild.url.subscribe((urlSegments) => {
+            const url = urlSegments.map((segment) => segment.path).join('/');
+            // Here you can add conditions to check if the specific route is reached
+            if (url.includes('/buy')) {
+              // Do something when the specific route is reached
+              this.showMenu = false;
+            }
+          });
+          currentRoute = currentRoute.firstChild;
+        }
+      });
   }
 }
