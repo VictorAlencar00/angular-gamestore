76117b4be6de178f5f0cd10173677cf91d20a7a7
VictorAlencar00 <victordesousaa@gmail.com>
2024-07-16 12:13:49 -0300

    feat: functional carousel

--- src/app/discover/carousel/carousel.component.ts
+++ src/app/discover/carousel/carousel.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Game } from '../../game.dto';
+import { GamesService } from './../../games.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'carousel',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
-export class CarouselComponent {}
+export class CarouselComponent implements OnInit {
+  games: Game[] = [];
+  highlightedGame: Game | null = null;
+
+  constructor(public gamesService: GamesService) {}
+  ngOnInit(): void {
+    this.gamesService.getGames().subscribe((data: Game[]) => {
+      this.games = data.slice(0, 5);
+      this.highlightedGame = this.games[0];
+    });
+  }
+
+  selectGame(game: Game) {
+    this.highlightedGame = game;
+  }
+}
