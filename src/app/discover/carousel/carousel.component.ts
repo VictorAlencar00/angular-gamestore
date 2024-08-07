5a1a0571080bf25b44d9ee73ae2ead4bcdcccf26
isaiasramosdeoliveira <isaias.ramos.olive@gmail.com>
2024-08-06 22:36:51 -0300

    refactor:  changing the carousel image

--- src/app/discover/carousel/carousel.component.ts
+++ src/app/discover/carousel/carousel.component.ts
@@ -1,7 +1,7 @@
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Game } from '../../game.dto';
 import { GamesService } from './../../games.service';
-import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'carousel',
@@ -21,11 +21,24 @@ export class CarouselComponent implements OnInit {
   }
 
   async getCarousel(): Promise<void> {
-    this.gamesService.getGames().subscribe((data: Game[]) => {
-      this.games = data.slice(0, 5);
-      this.highlightedGame = this.games[0];
-      this.alternateGame(this.games[0]);
-    });
+    const carousel = globalThis.document?.querySelector('#carousel');
+    if (carousel) {
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            this.alternateGame(this.games[0]);
+            this.selectGame(this.games[0]);
+          } else {
+          }
+        });
+      });
+
+      observer.observe(carousel);
+      this.gamesService.getGames().subscribe((data: Game[]) => {
+        this.games = data.slice(0, 5);
+        this.highlightedGame = this.games[0];
+      });
+    }
   }
 
   selectGame(game: Game) {
@@ -34,15 +47,15 @@ export class CarouselComponent implements OnInit {
   }
 
   alternateGame(game: Game) {
-    // if (this.timeoutId) {
-    //   clearTimeout(this.timeoutId);
-    // }
-    // this.timeoutId = setInterval(() => {
-    let nextGame = Number(game?.id) + 1;
-    if (nextGame > 4) {
-      nextGame = 0;
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId);
     }
-    this.selectGame(this.games[nextGame]);
-    // }, 3000);
+    this.timeoutId = setInterval(() => {
+      let nextGame = Number(game?.id) + 1;
+      if (nextGame > 4) {
+        nextGame = 0;
+      }
+      this.selectGame(this.games[nextGame]);
+    }, 3000);
   }
 }
