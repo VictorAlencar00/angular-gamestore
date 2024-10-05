aab677488de9d2c923e5a48a573ccfff88e74cc0
VictorAlencar00 <victordesousaa@gmail.com>
2024-10-05 13:27:02 -0300

    feat: game page wishlist button now works

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -18,20 +18,83 @@ export class GameComponent implements OnInit {
   constructor(public spinner: LoadingSpinnerService) {}
   paymentMehod: string = '';
 
-  public namedGame: Game | null = null;
+  public chosenGame: Game | null = null;
   private gamesService = inject(GamesService);
   private route: ActivatedRoute = inject(ActivatedRoute);
   listedGame: any | null = null;
   observableGame!: Observable<Game>;
+  isChosenGameLiked: boolean = false;
+
+  public likedGamesFromStorage: Game[] = [];
 
   async ngOnInit(): Promise<void> {
     this.spinner.showLoadingSpinner();
     const gameName: string = this.route.snapshot.paramMap.get('name')!;
     this.observableGame = this.gamesService.getGameByName(gameName);
     this.listedGame = await lastValueFrom(this.observableGame);
-    this.namedGame = this.listedGame[0];
+    this.chosenGame = this.listedGame[0];
     setTimeout(() => {
       this.spinner.hideLoadingSpinner();
     }, 300);
+
+    if (typeof window !== 'undefined' && window.localStorage) {
+      this.likedGamesFromStorage = JSON.parse(
+        window.localStorage.getItem('likedGames') || '[]',
+      );
+      if (
+        this.likedGamesFromStorage.some(
+          (likedGame) => likedGame.id === this.chosenGame?.id,
+        )
+      ) {
+        this.isChosenGameLiked = true;
+      }
+    }
   }
+
+  likeOrDislikeGame() {
+    const isGameAlreadyLiked = this.likedGamesFromStorage.some(
+      (likedGame) => likedGame.id === this.chosenGame?.id,
+    );
+
+    this.likedGamesFromStorage = isGameAlreadyLiked
+      ? (this.likedGamesFromStorage = this.likedGamesFromStorage.filter(
+          (likedGame) => likedGame.id !== this.chosenGame?.id,
+        ))
+      : [...this.likedGamesFromStorage, this.listedGame[0]];
+
+    window.localStorage.setItem(
+      'likedGames',
+      JSON.stringify(this.likedGamesFromStorage),
+    );
+
+    this.isChosenGameLiked = !this.isChosenGameLiked;
+  }
+
+  // likeOrDislikeGame() {
+  //   if (
+  //     this.likedGamesFromStorage.some(
+  //       (likedGame) => likedGame.id === this.chosenGame?.id,
+  //     )
+  //   ) {
+  //     this.likedGamesFromStorage = this.likedGamesFromStorage.filter(
+  //       (likedGame) => likedGame.id !== this.chosenGame?.id,
+  //     );
+  //     window.localStorage.setItem(
+  //       'likedGames',
+  //       JSON.stringify(this.likedGamesFromStorage),
+  //     );
+  //     this.isChosenGameLiked = false;
+  //   } else if (
+  //     !this.likedGamesFromStorage.some(
+  //       (likedGame) => likedGame.id === this.chosenGame?.id,
+  //     )
+  //   ) {
+  //     this.likedGamesFromStorage?.push(this.listedGame[0]);
+  //     window.localStorage.setItem(
+  //       'likedGames',
+  //       JSON.stringify(this.likedGamesFromStorage),
+  //     );
+  //     this.isChosenGameLiked = true;
+  //   }
+  // }
 }
