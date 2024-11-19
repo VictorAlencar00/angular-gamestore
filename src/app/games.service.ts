9682a0008619eafddfbd6ea893435d7141980b6d
VictorAlencar00 <victordesousaa@gmail.com>
2024-11-19 12:21:14 -0300

    fix: environment import fix

--- src/app/games.service.ts
+++ src/app/games.service.ts
@@ -2,7 +2,7 @@ import { Game } from './game.dto';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { finalize, Observable, tap } from 'rxjs';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root',
